2a89c371ab78ba56cc7d5c7ca085b9d2998ccc01
krupukcabe <sarofahmu@gmail.com>
2024-12-12 17:59:07 +0700

    menambahkan folder create.blade dan perintahnya

--- resources/views/Promo/create.blade.php
+++ resources/views/Promo/create.blade.php
@@ -34,7 +34,7 @@
             </div>
 
             <div class="mb-3">
-                <label for="discount" class="form-label">Diskon (%)</label>
+                <label for="discount" class="form-label">Diskon(%)</label>
                 <input type="number" name="discount" id="discount" class="form-control" value="{{ old('discount') }}" required>
             </div>
 
