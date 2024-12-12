a36eadff45e4d1dd2e561844ecdcbb4e680a3b93
mrizky <muhammadrizky23ti@mahasiswa.pcr.ac.id>
2024-12-12 20:10:37 +0700

    penambahan route untuk contact

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuestionAnswerController;
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('kontak', function () {
-    return view('kontak.contact');
-});
+Route::resource('contact', ContactController::class)->only(['index', 'store']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -22,4 +21,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resource('question_answer', QuestionAnswerController::class)->only(['index','store','edit','update','destroy']);
 require __DIR__.'/auth.php';
