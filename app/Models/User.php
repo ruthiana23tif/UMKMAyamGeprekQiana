558910777597232f3b5baa5c4eaf74c403deb5b8
mrizky <muhammadrizky23ti@mahasiswa.pcr.ac.id>
2024-12-12 20:09:13 +0700

    mengedit sedikit

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany; 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -11,7 +12,10 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+    public function QuestionAnswer(): HasMany
+    {
+        return $this->hasMany(QuestionAnswer::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
